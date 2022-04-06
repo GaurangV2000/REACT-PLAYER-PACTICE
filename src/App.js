8a7fc7970444cd8207aedacce2423c1f1d21d8e7
GaurangV2000 <gaurangvasoya1234@gmail.com>
2022-04-06 05:39:05 -0700

    incomplete

--- src/App.js
+++ src/App.js
@@ -1,25 +1,75 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import ReactPlayer from "react-player";
+import useObjectURL from "use-object-url";
+import "./App.css";
 
-function App() {
+const App = () => {
+  const [url, setUrl] = useState("");
+  const [chan, setChan] = useState("");
+  const [valid, setValid] = useState(false);
+  // const [burl, setBurl] = useState("");
+  // const eURL = encodeURIComponent(url);
+
+  const updateurl = () => {
+    var blob = new Blob([url]);
+
+    const uprl = URL.createObjectURL(blob);
+    // const revokedu = URL.revokeObjectURL(uprl);
+    // setBurl(revokedu);
+    console.log(uprl);
+  };
+
+  useEffect(() => {
+    if (valid) {
+      updateurl();
+    }
+  }, [url]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <h1> React Video Player</h1>
+
+      <div className="input-group mb-3  inputfieldp">
+        <input
+          type="url"
+          className="form-control"
+          value={chan}
+          onChange={(e) => setChan(e.target.value)}
+          placeholder="Recipient's username"
+        />
+        <button
+          className="btn btn-outline-secondary"
+          type="submit"
+          onClick={() => {
+            setUrl(chan);
+            setValid(true);
+            setChan("");
+          }}
+          id="button-addon2"
         >
-          Learn React
-        </a>
-      </header>
+          Button
+        </button>
+        {/* {console.log(eURL)} */}
+      </div>
+      {/* {console.log(burl)} */}
+
+      {valid ? (
+        <ReactPlayer
+          height="500px"
+          width="1000px"
+          controls
+          url={url}
+          // onReady={() => setValid(true)}
+          // onStart={() => console.log("vieo is Started")}
+          // onPause={() => console.log("vieo is Paused")}
+          // onEnded={() => console.log("vieo is Ended")}
+          onError={() => setValid(false)}
+        />
+      ) : (
+        <p>Your URL {url} is Invalid ! Please enter a Valid Url </p>
+      )}
     </div>
   );
-}
+};
 
 export default App;
